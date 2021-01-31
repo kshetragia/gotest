87baf3cecc83b42f88771a7c7ef0ad12159a0e6a
kshetragia <kshetragia@gmail.com>
2021-01-31 17:40:44 +0700

    Add Module32First and Module32Next winapi support

---
+++ internal/winapi/errors.go
@@ -0,0 +1,23 @@
+// +build windows
+
+package winapi
+
+import "syscall"
+
+const (
+	errnoERROR_IO_PENDING = 997
+)
+
+var (
+	errERROR_IO_PENDING error = syscall.Errno(errnoERROR_IO_PENDING)
+)
+
+func errnoErr(e syscall.Errno) error {
+	switch e {
+	case 0:
+		return nil
+	case errnoERROR_IO_PENDING:
+		return errERROR_IO_PENDING
+	}
+	return e
+}
